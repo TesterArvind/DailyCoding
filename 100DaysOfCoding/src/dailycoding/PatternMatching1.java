b7689c3cceec96cffa9cb9fe4f0b044060e70638
Admin <Admin@192.168.1.7>
2020-10-03 00:37:06 +0530

    Added pattern matching programs 1

---
+++ 100DaysOfCoding/src/dailycoding/PatternMatching1.java
@@ -0,0 +1,19 @@
+package dailycoding;
+
+//*
+//**
+//***         print pattern matching
+//****
+
+public class PatternMatching1 {
+
+	public static void main(String[] args) {
+		for(int i=0; i< 4; i++){
+			for(int j=0; j<i+1; j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
