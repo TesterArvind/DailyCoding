d54e73305e9397cc50d23c218431e6914188667b
Admin <Admin@192.168.1.7>
2020-10-03 14:14:46 +0530

    Added Sum Of Array

---
+++ 100DaysOfCoding/src/dailycoding/SumOfArray.java
@@ -0,0 +1,14 @@
+package dailycoding;
+
+public class SumOfArray {
+
+	public static void main(String[] args) {
+		int a[] = {2,2,2,2,2};
+		int sum=0;
+		for (int i = 0; i < a.length; i++) {
+			sum+=a[i];
+		}
+        System.out.println(sum);
+	}
+
+}
